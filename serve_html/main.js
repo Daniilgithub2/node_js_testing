af93412bd7f62ea3fb37841b0e38a8a77aa56e48
Daniilgithub2 <123155312+Daniilgithub2@users.noreply.github.com>
2023-09-28 09:17:00 +0300

    .

--- serve_html/main.js
+++ serve_html/main.js
@@ -1,25 +1,26 @@
-const port = 3000
-    http = require("http")
+const port = 3000,
+    http = require("http"),
     httpStatus = require("http-status-codes"),
-    fs = require("fs")
+    fs = require("fs");
 
-const routeMap = {
-    "/" : "views/index.html"
+const getViewUrl = (url) => {
+    return `views${url}.html`;
 };
 
-http
-    .createServer((req, res) => {
-        res.writeHead(200, {
-            'Content-Type' : 'text/html'
-        });
-        if (routeMap[req.url]) { 
-            fs.readFile(routeMap[req.url], (error, data) => {
+http.createServer((req, res) => {
+        let viewUrl = getViewUrl(req.url);
+        fs.readFile(viewUrl, (error, data) => {
+            if (error) {
+                res.writeHead(httpStatus.NOT_FOUND);
+                res.write('<h1>FILE NOT FOUND</h1>')
+            } else {
+                res.writeHead(httpStatus.OK, {
+                    'Content-Type': 'text/html'
+                });
                 res.write(data);
-                res.end();
-            });
-        } else {
-            res.end('<h1>Sorry, not found.</h1>');
-        }
+            }
+            res.end()
+        });
     })
     .listen(port);
 console.log(`The server has started and is listening on port number: ${port}`);
