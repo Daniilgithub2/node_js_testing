9fef09c5331f412b0b57e9960196f46a10579ddd
Daniilgithub2 <123155312+Daniilgithub2@users.noreply.github.com>
2023-10-04 22:29:38 +0300

    9.1

--- express_js_development/first_express_project/index.js
+++ express_js_development/first_express_project/index.js
@@ -4,7 +4,18 @@ const port = 3000,
 
 app.get('/', (req, res) => {
     res.send("Hello, Universe!");
-})  
+})
 .listen(port, () =>{
     console.log(`The Express.js server has started and is listening on port number: ${port}`);
+});
+app.post('/contact', (req, res) => {
+    res.send('Contact information submitted successfully.');
+});
+app.use((req, res, next) => {
+    console.log(`request made to: ${req.url}`);
+    next();
+});
+app.get("/items/:vegetable", (req, res) => {
+    let veg = req.params.vegetable;
+    res.send(`This is a page for ${veg}`);
 });
