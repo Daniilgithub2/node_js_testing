64918fb61496f0f499bdff4cbd0b6a7ddd9e4f0b
Daniilgithub2 <123155312+Daniilgithub2@users.noreply.github.com>
2023-10-05 09:50:45 +0300

    9.2

--- express_js_development/first_express_project/index.js
+++ express_js_development/first_express_project/index.js
@@ -2,20 +2,20 @@ const port = 3000,
     express = require('express'),
     app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hello, Universe!");
-})
-.listen(port, () =>{
-    console.log(`The Express.js server has started and is listening on port number: ${port}`);
-});
-app.post('/contact', (req, res) => {
-    res.send('Contact information submitted successfully.');
-});
+
 app.use((req, res, next) => {
     console.log(`request made to: ${req.url}`);
     next();
 });
-app.get("/items/:vegetable", (req, res) => {
-    let veg = req.params.vegetable;
-    res.send(`This is a page for ${veg}`);
+
+app.use(
+    express.urlencoded({
+        extended: false
+    })
+);
+
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
 });
