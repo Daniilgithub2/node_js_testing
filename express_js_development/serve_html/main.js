208b34b3230f53aea7338a479d37a2b1c136aa36
Daniilgithub2 <123155312+Daniilgithub2@users.noreply.github.com>
2023-10-12 00:22:23 +0300

    compliting chapter 10

--- express_js_development/serve_html/main.js
+++ express_js_development/serve_html/main.js
@@ -2,7 +2,24 @@ const port = 3000,
     express = require('express'),
     app = express();
 
-const homeController = require('./ocntrollers/homeController');
+const homeController = require('./controllers/homeController');
+const errorController = require('./controllers/errorController');
+const layouts = require("express-ejs-layouts")
 
+app.set("view engine", 'ejs')
+
+app.get('/', (req, res) => {
+    res.send("POST successfull!");
+});
 
 app.get("/items/:vegetable", homeController.sendReqParam);
+app.get("/name/:myName",homeController.respondWithName);
+
+app.use(layouts);
+app.use(errorController.logErrors);
+app.use(errorController.respondNoResourceFound);
+app.use(errorController.respondInternalError);
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
