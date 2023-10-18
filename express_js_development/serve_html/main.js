773e42d29ad2d4ec937d57aba2a015eda48aaa95
Daniilgithub2 <123155312+Daniilgithub2@users.noreply.github.com>
2023-10-19 02:46:29 +0300

    15.6

--- express_js_development/serve_html/main.js
+++ express_js_development/serve_html/main.js
@@ -5,12 +5,35 @@ const port = 3000,
 const homeController = require('./controllers/homeController');
 const errorController = require('./controllers/errorController');
 const layouts = require("express-ejs-layouts")
+const mongoose = require("mongoose");
+
+mongoose.connect(
+    "mongodb://localhost:27017/recipe_db",
+    {useNewUrlParser: true}
+);
+
+const db = mongoose.connection;
+
+db.once("open", () => {
+    console.log("Successfully connected to MongoDB using Mongoose!");
+});
+
+
+const subscribersController = require("./controllers/subscribersController");
 
 app.set("view engine", 'ejs')
 
 app.get('/', (req, res) => {
     res.send("POST successfull!");
 });
+
+app.get("/subscribers", subscribersController.getAllSubscribers, (req, res, next) => {
+    console.log(req.data);
+    res.send(req.data);
+});
+
+app.get("/contact", subscribersController.getSubscriptionPage);
+app.post("/subscribe", subscribersController.saveSubscriber);
 
 app.get("/items/:vegetable", homeController.sendReqParam);
 app.get("/name/:myName",homeController.respondWithName);
